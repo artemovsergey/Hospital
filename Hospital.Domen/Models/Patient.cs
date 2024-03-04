451c02cb1dd47cee8c95ddd61949a8e2dac6f55c
artemov <artik3314@gmail.com>
2024-03-04 20:37:43 +0300

    .

---
+++ Hospital.Domen/Models/Patient.cs
@@ -0,0 +1,12 @@
+﻿namespace Hospital.Domen.Models;
+
+public class Patient
+{
+    public int Id { get; set; }
+
+    public string Surname { get; set; }
+
+    public string Name { get; set; }
+
+    public string Patronymic { get; set; }
+}
---
+++ Hospital.PatientFlowManagement/Program.cs
@@ -0,0 +1,35 @@
+using Hospital.PatientFlowManagement.Components;
+
+namespace Hospital.PatientFlowManagement
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            var app = builder.Build();
+
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+             
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseAntiforgery(); // для защиты от поддельных ресурсов
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
